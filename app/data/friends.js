2dec35365d06e75ca19b1d8abd9b322d122fda2b
John Nowinsky <armthepit@armthepit.com>
2016-11-02 10:53:04 -0500

    set up the data table for one friend in the friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+// ===============================================================================
+// DATA
+// Below data will hold all friends.
+// Initially its just set it to one friend.
+// ===============================================================================
+
+var friends = [
+	{
+	  "name":"Ahmed",
+	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+	  "scores":[
+	     5,
+	     1,
+	     4,
+	     4,
+	     5,
+	     1,
+	     2,
+	     5,
+	     4,
+	     1
+	   ]
+	}
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = tableArray;
