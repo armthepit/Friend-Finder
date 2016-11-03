e7b5e3f04bf246236140b509653020f45c246b27
John Nowinsky <armthepit@armthepit.com>
2016-11-03 15:20:59 -0500

    add api post route to handle the ajax request with function to step thru scores and find new friend

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -15,22 +15,41 @@ var friendsTable = require('../data/friends.js');
 // ROUTING
 // ===============================================================================
 
-module.exports = function (app) {
-	// API GET Requests
-	// Below code handles when users "visit" a page.
-	// In each of the below cases when a user visits a link
-	// ---------------------------------------------------------------------------
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // ---------------------------------------------------------------------------
 
-	app.get('/api/friends', function (request, result) {
-		result.json(friendsTable);
-	});
+    app.get('/api/friends', function(request, result) {
+        result.json(friendsTable);
+    });
 
-	app.post("/api/friends", function (request, result) {
-		newFriend = request.body;
-		console.log(newFriend);
-		friendsTable.push(newFriend);
-		console.log(friendsTable);
-	});
+    app.post("/api/friends", function(request, result) {
+        var you = request.body;
+        var newFriend = -1;
+        var newFriendScore = 100;
+        var currentFriendScore = 0;
+        for (i = 0; i < friendsTable.length; i++) {
+
+            if (you.sex != friendsTable[i].sex) {
+            	console.log(you.sex + ' ' + friendsTable[i].sex);
+                for (j = 0; j < you.scores.length; j++) {
+
+                    currentFriendScore = currentFriendScore + Math.abs(friendsTable[i].scores[j] - you.scores[j]);
+                }
+                if (currentFriendScore <= newFriendScore) {
+                    newFriend = i;
+                    newFriendScore = currentFriendScore;
+                }
+                currentFriendScore = 0;
+            }
+        }
+        friendsTable.push(you);
+        newFriendDetails = friendsTable[newFriend];
+        result.json(newFriendDetails);
+
+    });
 
 
 };
