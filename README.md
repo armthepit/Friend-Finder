3500f1f8614283f1304e22cd389192cc19a850fa
John Nowinsky <armthepit@armthepit.com>
2016-11-04 11:59:00 -0500

    Clean up code and correct any issues when survey submitted. Added screenshots of app

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Friend-Finder
-Friend Finder is basically a dating app.
+
+### Overview
+
+ Friend-Finder is a dating app. This full-stack site will take in results your users' surveys, then compare their answers with those from other users. The app will then display the name and picture of the user with the best overall match. 
+
+
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -30,10 +30,10 @@ module.exports = function(app) {
         var newFriend = -1;
         var newFriendScore = 100;
         var currentFriendScore = 0;
+        // Loop thru all friends in the friends table to identify the friend have the lowest score difference and then return friend so modal opens showing the friend.
         for (i = 0; i < friendsTable.length; i++) {
 
             if (you.sex != friendsTable[i].sex) {
-            	console.log(you.sex + ' ' + friendsTable[i].sex);
                 for (j = 0; j < you.scores.length; j++) {
 
                     currentFriendScore = currentFriendScore + Math.abs(friendsTable[i].scores[j] - you.scores[j]);
